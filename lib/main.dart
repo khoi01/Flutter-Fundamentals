5015c1f4aa826159302e9a6b1358608d3787fbb7
BuildTools <unconfigured@null.spigotmc.org>
2020-01-26 01:50:25 +0800

    Update main.dart
    1..Add Material Widget
    2.Add Material color
    3.Add textStyle properties inside Text Widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,20 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(Home());
 
-
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Hello world",
-              textDirection: TextDirection.ltr,),
+    return Material(
+      color: Colors.deepOrange,
+      child: Center(
+          child: Text(
+        "Hello Flutter",
+        textDirection: TextDirection.ltr,
+        style: TextStyle(
+            fontWeight: FontWeight.w500,
+            fontSize: 23.4,
+            fontStyle: FontStyle.italic),
+      )),
     );
   }
 }
