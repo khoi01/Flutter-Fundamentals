16fa328b83ffe6e0720ed7a17b5bb0980d6b1253
BuildTools <unconfigured@null.spigotmc.org>
2020-01-26 02:29:14 +0800

    scaffoldExample
    1.Add Scaffold to the Project
    
    Scaffold provide appbar and body.

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -1,6 +1,33 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+class ScaffoldExample extends StatelessWidget {
+  _tapButton() {
+    debugPrint("tapped button");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Scaffold"),
+        centerTitle: true,
+        backgroundColor: Colors.deepOrange.shade500,
+        actions: <Widget>[
+          IconButton(
+              icon: Icon(Icons.email),
+              onPressed: () => debugPrint("email trigger")),
+          IconButton(
+              icon: Icon(Icons.access_alarm), onPressed: () => _tapButton())
+        ],
+      ),
+      body: Center(
+        child: Text("Hello Again"),
+      ),
+    );
+  }
+}
+
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
