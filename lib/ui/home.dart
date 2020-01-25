67c3efb9cbe38fb512fbcd1df1d190a09db21ab8
BuildTools <unconfigured@null.spigotmc.org>
2020-01-26 01:56:41 +0800

    separate file
    Our project getting bigger. We will separate source code to difference file.. so its become more readable later in the future.

---
+++ lib/ui/home.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.deepOrange,
+      child: Center(
+          child: Text(
+        "Hello Flutter",
+        textDirection: TextDirection.ltr,
+        style: TextStyle(
+            fontWeight: FontWeight.w500,
+            fontSize: 23.4,
+            fontStyle: FontStyle.italic),
+      )),
+    );
+  }
+}
